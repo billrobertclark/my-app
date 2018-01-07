f820e52717873366f25f2079470986e50eff53d2
Bill Clark <billrobertclark@gmail.com>
2018-01-07 15:54:37 -0600

    WIP - Navbar Close

--- resources/assets/js/components/Common/Navigation.vue
+++ resources/assets/js/components/Common/Navigation.vue
@@ -11,15 +11,53 @@
 		     aria-label="main navigation">
 		    <div class="container">
 				<div class="navbar-brand">
-					<a class="" href="/">
-						<img src="http://via.placeholder.com/250x53">
+					<a class="logo" href="/">
+						<img src="/img/logo.png">
 					</a>
 
-					<button class="button navbar-burger">
+					<button class="button navbar-burger"
+							@click="menuActive = !menuActive">
 						<span></span>
 						<span></span>
 						<span></span>
 					</button>
+				</div>
+				<div class="navbar-menu is-hidden-tablet"
+				     :class="{ 'is-active': menuActive }">
+				    <div class="search search-mobile field has-addons">
+				    	<div class="control">
+				    		<input class="input" type="text" placeholder="Whatc'ha want to learn?">
+				    	</div>
+				    	<div class="control">
+				    		<a class="button is-info">
+				    			Search
+				    		</a>
+				    	</div>
+				    </div>
+					<div class="navbar-item">
+						<a href="#">SERIES</a>
+					</div>
+					<div class="navbar-item">
+						<a href="#">CASTS</a>
+					</div>
+					<div class="navbar-item">
+						<a href="#">TOOLS</a>
+					</div>
+					<div class="navbar-item">
+						<a href="#">DISCUSS</a>
+					</div>
+				</div>
+				<div class="navbar-start is-hidden-touch">
+					<div class="search search-desktop field has-addons">
+						<div class="control">
+							<input class="input" type="text" placeholder="Whatc'ha want to learn?">
+						</div>
+						<div class="control">
+							<a class="button is-info">
+								Search
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</nav>
@@ -28,16 +66,88 @@
 
 <script>
 	export default {
-		props: ['state']
-	}
+		props: ['state'],
+		data() {
+			return {
+				menuActive: false
+			}
+		}
+}
 </script>
 
 <style lang="scss">
-	.navbar-burger {
+	.navbar-burger.button {
 		border: none;
+		height: 5rem;
 		& span {
 			margin: 0;
 		}
+		&:active {
+			background: green;
+		}
+		&:focus {
+			background: none;
+			box-shadow: none;
+		}
+		&:hover {
+			background: none;
+		}
+	}
+
+	.navbar-menu {
+		box-shadow: 0 3px 5px 0 #c1c1c1;
+		border-top: 1px solid #cdcdcd;
+		& > div.navbar-item {
+			margin: 1em 1.5em 1em 1.5em;
+			padding-left: 1em;
+			border-bottom: 1px solid #cdcdcd;
+		}
+		& > div.navbar-item:last-child {
+			border-bottom: none;
+		}
+	}
+
+	.navbar-start {
+		flex-grow: 1;
+	}
+
+	.search {
+		&.field.has-addons {
+			width: 100% !important;
+		}
+		& .control .input[type="text"] {
+			margin: 0;
+			border-radius: 20px 0 0 20px !important;
+			border-right: none;
+			font-size: 1.3em;
+			font-weight: bold;
+		}
+		& .control:nth-child(2) {
+			border: solid 2px #cdcdcd;
+			border-left: none;
+			border-radius: 0 20px 20px 0;
+			& .button {
+				height: 95%;
+				margin: 1px 1px 1px 0;
+				border-radius: 4px 17px 17px 4px !important;
+			}
+		}
+		&-desktop {
+			padding: 1em 2em;
+			& .control:first-child {
+				width: 70%;
+			}
+		}
+		&-mobile {
+			padding: 1em 5%;
+			& .control:first-child {
+				flex-grow: 1;
+			}
+		}
+	}
+
+	.logo {
+		margin: 15px 0px 0px 10px;
 	}
 
 	.is-home {
